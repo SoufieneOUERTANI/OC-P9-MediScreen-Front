9a11684e4cc729b899f83c9678e42b3d6e61cc21
Soufiene OUERTANI <soufiene.ouertani@gmail.com>
2021-11-26 10:39:39 +0100

    Root routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PatientListComponent } from './patient-list/patient-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'patients', component: PatientListComponent },
+  { path: '', redirectTo: 'patients', pathMatch: 'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
