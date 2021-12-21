be2ee75820c9bd3103342d3c404ef6e84314d0a5
Soufiene OUERTANI <soufiene.ouertani@gmail.com>
2021-12-21 16:30:09 +0100

    App config for OpenID Connect

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oa3ea09peJfiwnYP5d7',
+    issuer: 'https://dev-49078090.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
